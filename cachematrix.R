bd21171fae7b693e4e5c8a58a1f1b4c4d363d032
rahuldcosta <rahuldc999@gmail.com>
2014-07-21 23:10:43 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching and keeping a previously computer Inverse of A Matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a matrix,also there are fucntions  get ,set,setinv(to set inverse)
+## getinv(to get inverse)
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) m <<- inverse
+  getinv <- function() m
+  
+  list(set = set,get = get,setinv = setinv, getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes the matrix and produces a Inverse of the Matrix
+## I am using TryCatch to Handle The Error is a controlled fashion
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  out <- tryCatch(
+    
+{
+  
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m<-solve(data);
+  
+  x$setinv(m)
+  print(m)
+},
+
+error=function(cond) {
+  print("Error :YOur Matrix has no Inverse MAtrix ,Please Enter a DIfferent Matrix");
+  
+}
+  )
 }
